548cf24e83a91ee0b74c36b2759ad4c89c1b0f21
felipe_sbs <felipe.soares@stctecnologia.com.br>
2024-09-28 18:03:51 -0300

    feat: url path in index and imagem

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from galeria.views import index, index_outra_maneira, imagem
 
 urlpatterns = [
-    path("", index),
+    path("", index, name='index'),
     path("index2", index_outra_maneira),
-    path("imagem/", imagem),
+    path("imagem/", imagem, name='imagem'),
 ]
