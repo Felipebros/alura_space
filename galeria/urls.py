ebb483271a780e83ced1594f2f94f6bdb9b24142
felipe_sbs <felipe.soares@stctecnologia.com.br>
2024-09-28 11:09:04 -0300

    feat: index_outra_maneira

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, index_outra_maneira
 
 urlpatterns = [
     path("", index),
+    path("index2", index_outra_maneira),
 ]
