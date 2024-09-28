1ddb83c7bc8e40adafe7e9f70cc17bbd0a75a2b9
felipe_sbs <felipe.soares@stctecnologia.com.br>
2024-09-28 17:43:35 -0300

    feat: static files in index

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "galeria/index.html")
 
 
 def index_outra_maneira(request):
